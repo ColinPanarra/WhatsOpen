706a93ec7634cc78857304f7c2c6e6f26eab124d
Colin Panarra <51117182+ColinPanarra@users.noreply.github.com>
2019-07-18 16:22:31 -0400

    Delete send.js

--- send.js
+++
@@ -1,6 +0,0 @@
-var database = firebase.database();
-
-var openStoresRef = firebase.database().ref('stores/')
-  openStoresRef.on('value', function(snapshot) {
-    updateOpenStores(postElement, snapshot.val()); 
-  })
