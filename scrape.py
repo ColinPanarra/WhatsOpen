5906a06e7e1d7388bef44aa444b609779d2bcfb9
Colin Panarra <51117182+ColinPanarra@users.noreply.github.com>
2019-09-05 13:35:21 -0400

    got it only scraping open stores, still need to only get name of store

--- scrape.py
+++ scrape.py
@@ -8,11 +8,22 @@ from selenium import webdriver
 
 url = 'https://brandeis.sodexomyway.com/dining-near-me/open-now'
 
-browser= webdriver.Chrome(executable_path=r'C:/Users/colin/AppData/Local/Programs/Python/Python37-32/Lib/site-packages/selenium/webdriver/chrome')
+browser= webdriver.Chrome(executable_path='C:/Users/colin/AppData/Local/Programs/Python/Python37-32/Lib/site-packages/selenium/webdriver/chrome/chromedriver.exe')
 browser.get(url)
 
 html = browser.page_source
 
 soup = BeautifulSoup(html, 'lxml')
-stores = soup.find('div', class_='dining-halls-container')
-print(store)
+
+#need to create a lambda function so that it finds specific dining-hall-container WITHOUT 'hide'
+result = soup.find_all(lambda tag: tag.name == 'div' and tag.get('class') == ['dining-halls-container'])
+
+for h2 in result:
+    h2tag = h2.find_all('div', class_='dining-halls-block')
+    print(h2[0].text)
+
+
+#
+#stores = result.select('h2')
+#print(stores)
+#
